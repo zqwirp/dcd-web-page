cd8be16c2fca921dcf1259e57ba2a6122bb7257a
wiji <wiji.permana@gmail.com>
2022-06-06 13:24:46 +0700

    Update

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const toggleMenuButton = document.getElementById("toggle-menu-button");
 
 toggleMenuButton.addEventListener("click", event => {
   const navbarLinks = document.getElementById("navbar-links");
-
   navbarLinks.classList.toggle("active");
+});
+
+window.addEventListener("resize", event => {
+  const navbarLinks = document.getElementById("navbar-links");
+  if (window.innerWidth >= 800) {
+    navbarLinks.style.transition = "none";
+    navbarLinks.classList.remove("active");
+  } else {
+    navbarLinks.style.transition = "transform 1s";
+  }
 });
